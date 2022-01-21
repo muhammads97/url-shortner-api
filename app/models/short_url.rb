4da177bcaba57409ca59c15bdb1e61127a8b8453
Muhammad Salah <msalah.29.10@gmail.com>
2022-01-21 19:40:08 +0200

    adding url validation

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,4 +1,5 @@
 class ShortUrl < ApplicationRecord
+  require 'uri'
 
   CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
 
@@ -13,6 +14,19 @@ class ShortUrl < ApplicationRecord
   private
 
   def validate_full_url
+    if full_url.length == 0
+      errors.add(:full_url, "can't be blank")
+    else
+      begin
+        url = URI.parse(full_url)
+        valid = url.kind_of?(URI::HTTP) || url.kind_of?(URI::HTTPS)
+      rescue 
+        valid = false
+      end
+      errors.add(:full_url, "is not a valid url") if !valid
+    end
+
   end
+  
 
 end
